10975ac6476afa3497d82ae83fc46b19c7e7c428
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-27 21:59:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   ["kiwi"].join(",")
-  # ["kiwi", "durian"].jion(and)
+   
 end
 
-# def oxford_comma(array)
+# def oxford_comma()
 #   array=["kiwi", "durian"].jion("and")
 # end
